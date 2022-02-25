7b98346597309fd692129bc9fa88f5c151f8baa0
bicsu <bicsu@naver.com>
2022-02-25 21:28:58 +0900

    first

---
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,35 @@
+# /app.py
+from flask import Flask, render_template, request
+from help_funcs import get_date, get_num, lottodataUpdate
+from flask import jsonify
+#Flask 객체 인스턴스 생성
+app = Flask(__name__)
+
+@app.route('/',methods=('GET', 'POST')) #
+def index():
+
+    # show today and this saturday dates
+
+    today_, saturday_ = get_date()
+
+    if request.method == "POST":
+        # user=request.form['user'] # 전달받은 name이 user인 데이터
+        print(request.form.get('user')) 
+        return render_template('index.html', today_=today_, satur=saturday_)
+    elif request.method == "GET":
+        return render_template('index.html', today_=today_, satur=saturday_)
+
+
+@app.route('/show_num',methods=('GET', 'POST')) # 접속하는 url
+def show_num():
+    sggstd_ltt_num = get_num()
+    return render_template('show_num.html', suggested_num=sggstd_ltt_num)
+
+@app.route('/update_round',) # 접속하는 url
+def update_round():
+    lottodataUpdate()
+    return render_template('index.html')
+if __name__=="__main__":
+    app.run(debug=True)
+    # host 등을 직접 지정하고 싶다면
+    # app.run(host="127.0.0.1", port="5000", debug=True)
---
+++ help_funcs.py
@@ -0,0 +1,111 @@
+import random
+from datetime import datetime
+from datetime import timedelta
+import statistics as stat
+import pandas as pd
+import requests
+import json
+from collections import Counter
+import pickle
+import os
+
+def get_num():
+    '''
+    1. description :
+        get 10 sets of lotto number
+
+    2. parameter 
+        x
+    3. returns
+        result_num_list : list, 10 lengths and 2 dimension list array
+    '''
+    result_num_list = []
+    while len(result_num_list) <10 :
+        ran_list = random.sample(range(1,46),6)
+        if stat.mean(ran_list) >= 20 and stat.mean(ran_list) < 25 :
+            ran_list.sort()
+            result_num_list.append(ran_list)
+        elif stat.stdev(ran_list) >= 12 or stat.stdev(ran_list) < 16 :
+            ran_list.sort()
+            result_num_list.append(ran_list)
+    now_ = datetime.now()
+    now_str = f'{now_.year}-{now_.month:02d}-{now_.day:02d}'
+    lotto_data = pd.read_csv("./data/lotto_data.csv")
+    latest_round = len(lotto_data)
+    result_list = [now_str]+result_num_list
+    if os.path.isfile(f'./data/nums_thisweek_{latest_round}.pkl') == False:
+        with open(f'./data/nums_thisweek_{latest_round}.pkl', 'wb') as f:
+            pickle.dump(result_list, f)
+    else : 
+        with open(f'./data/nums_thisweek_{latest_round}.pkl', 'rb') as f:
+            result_list = pickle.load(f)
+    return result_list[1:]
+
+
+def get_date():
+    '''
+    1. description :
+        get today and this saturday(lotto day)
+    2. parameter 
+        x
+    3. returns
+        today_str : str, yyyy-mm-dd
+        saturday : str, yyyy-mm-dd
+    '''
+    now_ = datetime.now()
+    today_str = f'{now_.year}-{now_.month}-{now_.day}'.format(now_.year)
+    d = datetime.today()    
+    sat_offset = 5 - d.weekday()
+
+    saturday = d + timedelta(days=sat_offset)    
+    saturday = str(saturday)[:10]
+    print("Next Saturday :", saturday)
+
+    return today_str, saturday
+
+
+
+
+def lottodataUpdate():
+    lotto_data = pd.read_csv("./data/lotto_data.csv")
+    minDrwNo = len(lotto_data)       #취득하고 싶은 회차 시작
+    maxDrwNo = len(lotto_data)+1        #취득하고 싶은 회차 종료
+    drwtNo1 = []        #1등 첫번째 번호
+    drwtNo2 = []        #1등 두번째 번호
+    drwtNo3 = []        #1등 세번째 번호
+    drwtNo4 = []        #1등 네번째 번호
+    drwtNo5 = []        #1등 다섯번째 번호
+    drwtNo6 = []        #1등 여섯번째 번호
+    bnusNo = []         #1등 보너스 번호
+    drwNoDate = []      #로또 추첨일
+
+    # 지정한 시작 회차 부터 종료 회차 까지 취득
+    try :
+        for i in range(minDrwNo, maxDrwNo + 1, 1):
+            # 1등 번호를 취득
+            req_url = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=" + str(i)
+
+            req_lotto = requests.get(req_url)
+
+            lottoNo = req_lotto.json()
+
+            drwNoDate.append(lottoNo['drwNoDate'])  # 로또 추첨일
+            drwtNo1.append(lottoNo['drwtNo1'])      #1등 첫번째 번호 저장
+            drwtNo2.append(lottoNo['drwtNo2'])      #1등 두번째 번호 저장
+            drwtNo3.append(lottoNo['drwtNo3'])      #1등 세번째 번호 저장
+            drwtNo4.append(lottoNo['drwtNo4'])      #1등 네번째 번호 저장
+            drwtNo5.append(lottoNo['drwtNo5'])      #1등 다섯번째 번호 저장
+            drwtNo6.append(lottoNo['drwtNo6'])      #1등 여섯번째 번호 저장
+            bnusNo.append(lottoNo['bnusNo'])   #1등 보너스 번호 저장    
+    except :
+        print("Round counting error please check data and check the current lotto round")
+
+    lotto_dict = {"추첨일":drwNoDate, "Num1":drwtNo1, "Num2":drwtNo2, "Num3":drwtNo3, "Num4":drwtNo4, "Num5":drwtNo5, "Num6":drwtNo6, "bnsNum":bnusNo,} 
+    df_lotto_toadd = pd.DataFrame(lotto_dict)
+
+    if len(df_lotto_toadd) != 0 :
+        lotto_data = pd.concat([lotto_data, df_lotto_toadd]).drop_duplicates()
+        lotto_data.to_csv(f"./data/lotto_data.csv", index=False)
+
+
+    print('New data updated Done')
